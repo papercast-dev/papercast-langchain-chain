a985769f5727119097bde282981a282018ab988b
Gabriel Simmons <simmons.gabe@gmail.com>
2023-04-11 12:57:13 -0700

    init

---
+++ papercast_langchain_chain/chain_processor.py
@@ -0,0 +1,19 @@
+from papercast.base import BaseProcessor
+from papercast.base import Production
+
+from langchain.chains import LLMChain
+
+
+class ChainProcessor(BaseProcessor):
+    input_types = {"text": str}
+    output_types = {"text": str}
+
+    def __init__(
+        self,
+        chain: LLMChain,
+    ):
+        self.chain = chain
+
+    def process(self, production: Production, **kwargs) -> Production:
+        setattr(production, "text", self.chain(getattr(production, "text")))
+        return production
